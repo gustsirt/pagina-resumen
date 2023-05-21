a83f9d25c2db219b15fcbf511df3094e5c557083
gustsirt <gustavo.sirtori@gmail.com>
2023-05-21 16:20:15 -0300

    arracando header y footer

---
+++ js/main.js
@@ -0,0 +1,22 @@
+  // Mobile nav toggle
+  
+  const granBody = document.getElementById('bodyy')
+  const mobileButton = document.getElementById('mobile-button')
+  const navbar = document.getElementById('navbar')
+  let estadonavbar = 1;
+
+function accionmobile(){
+  if (estadonavbar == 1) {
+    estadonavbar = 0
+    granBody.classList.add('mobile-nav-active')
+    mobileButton.classList.add('bi-x')
+    mobileButton.classList.remove('bi-list')
+  } else {
+    estadonavbar = 1
+    granBody.classList.remove('mobile-nav-active')
+    mobileButton.classList.add('bi-list')
+    mobileButton.classList.remove('bi-x')
+  }
+  }
+
+  mobileButton.addEventListener('click', accionmobile )
